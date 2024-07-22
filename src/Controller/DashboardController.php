c7cb6dc54af8716df1f83eb081c1f674c90f3979
Katie Elizabeth <32232155+pixiekat@users.noreply.github.com>
2024-07-21 20:41:28 -0400

    fixing code

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,7 +4,7 @@ namespace Pixiekat\MolecularTumorBoardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController {
   #[Route('/', name: 'mtb_dashboard')]
--- src/DependencyInjection/MolecularTumorBoardExtension.php
+++ src/DependencyInjection/MolecularTumorBoardExtension.php
@@ -12,5 +12,6 @@ class MolecularTumorBoardExtension extends Extension {
   public function load(array $configs, ContainerBuilder $container): void {
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
     $loader->load('services.yaml');
+    //$loader->load('routes.yaml');
   }
 }
