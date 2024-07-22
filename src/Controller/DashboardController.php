d2e502374c5f278608385b974c9bb068336053aa
Katie Elizabeth <32232155+pixiekat@users.noreply.github.com>
2024-07-21 20:15:13 -0400

    update route for dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractController {
-  #[Route('/molecular-tumor-board', name: 'mtb_dashboard')]
+  #[Route('/', name: 'mtb_dashboard')]
   public function index(): Response {
     return $this->render('@MolecularTumorBoardBundle/dashboard/index.html.twig', []);
   }
