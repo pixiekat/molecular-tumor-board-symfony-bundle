df4c31b8945e233dbb84cb959bb6a75ba6eafba3
Katie Elizabeth <32232155+pixiekat@users.noreply.github.com>
2024-08-22 07:49:44 -0400

    add cancertypemanager

---
+++ src/Services/CancerTypeManager.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+namespace Pixiekat\MolecularTumorBoard\Services;
+use Pixiekat\MolecularTumorBoard\Services\Interfaces;
+
+class CancerTypeManager implements Interfaces\CancerTypeManagerInterface {
+}
